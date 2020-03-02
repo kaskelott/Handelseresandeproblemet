e0447644182841f58efd7d59671f848023721211
kaskelott <17jens.kollberg@elev.kitas.se>
2020-03-02 19:15:56 +0100

    Add project files.

---
+++ WpfApp3/Indata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    class Indata
+    {
+        public Tuple<int,int> Koordinater { get; set; }
+        public int Grupp { get; set; }
+    }
+}
